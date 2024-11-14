b8ee1b076038ecc19edf60b2fe04b6d717fe1220
Ralica Popova <ralicaterzieva90@gmail.com>
2024-11-14 18:04:18 +0200

    Style home page with Tailwindcss

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
 export default function Homepage() {
-  return <div>Homepage</div>
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Homepage</h1>
+    </div>
+  );
 }
