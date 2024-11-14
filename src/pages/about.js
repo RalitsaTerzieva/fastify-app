39ce9cd4507bb0731dc61dd0bf53398732bab5f5
Ralica Popova <ralicaterzieva90@gmail.com>
2024-11-14 18:06:00 +0200

    Style about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,7 @@
 export default function About() {
-    return <div>This about page is served from the Next.js</div>
-}
+    return (
+      <div className="bg-blue-100 text-center p-10 text-lg text-gray-800">
+        This about page is served from Next.js
+      </div>
+    );
+  }
